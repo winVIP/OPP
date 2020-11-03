1839202d89e659fcbb3f3cb92f6e4fb0f1511c30
jonaspetraska7 <jonas.petraska7@gmail.com>
2020-11-03 06:06:32 +0200

    Facade done'ish

---
+++ OPP/Facade.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OPP
+{
+    public class Facade
+    {
+        Game GameSystem;
+        Login LoginSystem;
+        Map MapSystem;
+
+        public Facade()
+        {
+            GameSystem = new Game();
+            LoginSystem = new Login();
+            MapSystem = new Map();
+        }
+
+        public void ConfigureApplicationAndStartLoginProccess()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(LoginSystem);
+        }
+
+        public string SendARequestandReturnAnswer(PictureBox playerPictureBox)
+        {
+            List<string> requestArgs = new List<string>();
+            requestArgs.Add(playerPictureBox.BackColor.Name);
+            requestArgs.Add("/set");
+            Get getreq = new Get("https://localhost:5001/api/game/rewind/", requestArgs);
+            return getreq.SendRequest();
+        }
+
+    }
+}
